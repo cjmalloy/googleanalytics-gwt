01dc4a532fef4ed7db92e6d6eeb690c77535b160
Chris Malloy <cjmalloy@gmail.com>
2017-03-12 18:37:29 -0300

    Updated checkstyle.

--- src/main/java/com/cjmalloy/analytics/client/GoogleAnalytics.java
+++ src/main/java/com/cjmalloy/analytics/client/GoogleAnalytics.java
@@ -1,58 +1,60 @@
 package com.cjmalloy.analytics.client;
 
-public class GoogleAnalytics
-{
+public class GoogleAnalytics {
 
-    public static native void sendEvent(String category, String action) /*-{
+  public static interface HitCallback {
 
-        $wnd.ga('send', 'event', category, action);
-     }-*/;
+    void call();
+  }
 
-    public static native void sendEvent(String category, String action, HitCallback callback) /*-{
+  public static native void sendEvent(String category, String action) /*-{
+    $wnd.ga('send', 'event', category, action);
+  }-*/;
 
-        $wnd.ga('send', 'event', category, action,
-            {'hitCallback': $entry(function(){callback.@com.cjmalloy.analytics.client.GoogleAnalytics.HitCallback::call()();})});
-     }-*/;
+  public static native void sendEvent(String category, String action, HitCallback callback) /*-{
 
-    public static native void sendEvent(String category, String action, String label) /*-{
+    $wnd.ga('send', 'event', category, action, {
+        hitCallback: $entry(function () {
+          callback.@com.cjmalloy.analytics.client.GoogleAnalytics.HitCallback::call()();
+        })
+      });
+  }-*/;
 
-        $wnd.ga('send', 'event', category, action, label);
-     }-*/;
+  public static native void sendEvent(String category, String action, String label) /*-{
+    $wnd.ga('send', 'event', category, action, label);
+  }-*/;
 
-    public static native void sendEvent(String category, String action, String label, HitCallback callback) /*-{
+  public static native void sendEvent(String category, String action, String label, HitCallback callback) /*-{
 
-        $wnd.ga('send', 'event', category, action, label,
-            {'hitCallback': $entry(function(){callback.@com.cjmalloy.analytics.client.GoogleAnalytics.HitCallback::call()();})});
-     }-*/;
+    $wnd.ga('send', 'event', category, action, label, {
+        hitCallback: $entry(function () {
+          callback.@com.cjmalloy.analytics.client.GoogleAnalytics.HitCallback::call()();
+        })
+      });
+  }-*/;
 
-    public static native void sendEvent(String category, String action, String label, int value) /*-{
+  public static native void sendEvent(String category, String action, String label, int value) /*-{
+    $wnd.ga('send', 'event', category, action, label, value);
+  }-*/;
 
-        $wnd.ga('send', 'event', category, action, label, value);
-     }-*/;
+  public static native void sendEvent(String category, String action, String label, int value, HitCallback callback) /*-{
 
-    public static native void sendEvent(String category, String action, String label, int value, HitCallback callback) /*-{
+    $wnd.ga('send', 'event', category, action, label, value, {
+        hitCallback: $entry(function () {
+          callback.@com.cjmalloy.analytics.client.GoogleAnalytics.HitCallback::call()();
+        })
+      });
+  }-*/;
 
-        $wnd.ga('send', 'event', category, action, label, value,
-            {'hitCallback': $entry(function(){callback.@com.cjmalloy.analytics.client.GoogleAnalytics.HitCallback::call()();})});
-     }-*/;
+  public static void trackHistoryEvent(String historyToken) {
+    sendEvent("gwt-history", "click", historyToken, 1);
+  }
 
-    public static void trackHistoryEvent(String historyToken)
-    {
-        sendEvent("gwt-history", "click", historyToken, 1);
-    }
+  public static void trackOutboundLink(String url) {
+    sendEvent("outbound", "click", url, 1);
+  }
 
-    public static void trackOutboundLink(String url)
-    {
-        sendEvent("outbound", "click", url, 1);
-    }
-
-    public static void trackOutboundLink(String url, HitCallback callback)
-    {
-        sendEvent("outbound", "click", url, 1, callback);
-    }
-
-    public static interface HitCallback
-    {
-        void call();
-    }
+  public static void trackOutboundLink(String url, HitCallback callback) {
+    sendEvent("outbound", "click", url, 1, callback);
+  }
 }
--- src/main/java/com/cjmalloy/analytics/client/ui/ExternalLink.java
+++ src/main/java/com/cjmalloy/analytics/client/ui/ExternalLink.java
@@ -8,41 +8,34 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.ui.Anchor;
 
-public class ExternalLink extends Anchor
-{
-    private HandlerRegistration clickReg = null;
+public class ExternalLink extends Anchor {
 
-    public ExternalLink() {}
+  private HandlerRegistration clickReg = null;
 
-    public ExternalLink(String url)
-    {
-        setHref(url);
+  public ExternalLink() {}
+
+  public ExternalLink(String url) {
+    setHref(url);
+  }
+
+  @Override
+  public void setHref(final String url) {
+    super.setHref(url);
+    if (clickReg != null) {
+      clickReg.removeHandler();
     }
-
-    @Override
-    public void setHref(final String url)
-    {
-        super.setHref(url);
-        if (clickReg != null)
-        {
-            clickReg.removeHandler();
-        }
-        clickReg = this.addClickHandler(new ClickHandler()
-        {
-            @Override
-            public void onClick(ClickEvent event)
-            {
-                event.stopPropagation();
-                event.preventDefault();
-                GoogleAnalytics.trackOutboundLink(url, new HitCallback()
-                {
-                    @Override
-                    public void call()
-                    {
-                        Location.assign(url);
-                    }
-                });
-            }
+    clickReg = this.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        event.stopPropagation();
+        event.preventDefault();
+        GoogleAnalytics.trackOutboundLink(url, new HitCallback() {
+          @Override
+          public void call() {
+            Location.assign(url);
+          }
         });
-    }
+      }
+    });
+  }
 }
